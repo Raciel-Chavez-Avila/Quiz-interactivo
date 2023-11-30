aec2e6ce70c9ceb9c37593e827a9d57abaae37de
RacielCA <raciel.chavez.uicslp.tmz@gmail.com>
2023-11-30 13:12:46 -0600

    first commit

---
+++ js/question.js
@@ -0,0 +1,58 @@
+let questions = [
+	{
+		numb: 1,
+		question: "¿Qué significa HTML?",
+		answer: "C. Hyper Text Markup Language",
+		options: [
+			"A. Hyper Type Multi Language",
+			"B. Hyper Text Multiple Language",
+			"C. Hyper Text Markup Language",
+			"D. Home Text Multi Language"
+		]
+	},
+	{
+		numb: 2,
+		question: "¿Qué significa CSS?",
+		answer: "A. Cascading Style Sheet",
+		options: [
+			"A. Cascading Style Sheet",
+			"B. Cute Style Sheet",
+			"C. Computer Style Sheet",
+			"D. Academy Style Sheet"
+		]
+	},
+	{
+		numb: 3,
+		question: "¿Qué significa PHP?",
+		answer: "A. Hypertext Preprocessor",
+		options: [
+			"A. Hypertext Preprocessor",
+			"B. Hometext Programming",
+			"C. Hypertext Preprogramming",
+			"D. Programming Hypertext Preprocessor"
+		]
+	},
+	{
+		numb: 4,
+		question: "¿Qué significa SQL?",
+		answer: "D. Structured Query Language",
+		options: [
+			"A. Strength Query Language",
+			"B. Styleshhet Query Language",
+			"C. Science Question Language",
+			"D. Structured Query Language"
+		]
+	},
+	{
+		numb: 5,
+		question: "¿Qué significa XML?",
+		answer: "D. Extensible Markup Language",
+		options: [
+			"A. Excellent Multiple Language",
+			"B. Explore Multiple Language",
+			"C. Extra Markup Language",
+			"D. Extensible Markup Language"
+		]
+	}
+
+];
